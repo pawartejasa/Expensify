8d99555ec202cd3ca402c24068e81700ea970815
pawartejasa <pawartejasa@gmail.com>
2021-06-08 19:18:08 +1000

    initial source

--- src/app.js
+++ src/app.js
@@ -12,14 +12,6 @@ import './styles/styles.scss';
 
 const store = ConfigureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 5400, createdAt:1000}));
-store.dispatch(addExpense({ description: 'Gas bill',amount: 54000  }));
-store.dispatch(addExpense({ description: 'Phone bill',amount: 540000  }));
-//store.dispatch(setTextFilter('water'));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (<Provider store={store}>
         <AppRouter />
